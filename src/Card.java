9b8ecbac576ff159a0eafea27183b11ee9dfb0ff
Darcie Browning <darcie.browning@my.jcu.edu.au>
2016-08-26 21:58:01 +1000

    Hard coded card values
    Hard coded card values, and created card class and extended the class
    to include mineral cards. Trump cards to be done.

---
+++ src/Card.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Darcie on 26/08/2016.
+ */
+
+public class Card {
+
+    public String cardTitle;
+    public String cardChemistry;
+    public String cardClassification;
+    public String cardCrystalSystem;
+    public String cardOccurrence;
+    public double cardHardness;
+    public double cardSpecificGravity;
+    public String cardCleavage;
+    public String cardCrystalAbundance;
+    public String cardEconomicValue;
+
+    public Card(String title, String chemistry, String classification,
+                String crystalSystem, String occurrence, double hardness,
+                double specificGravity, String cleavage, String crystalAbundance,
+                String economicValue){
+        cardTitle = title;
+        cardChemistry = chemistry;
+        cardClassification = classification;
+        cardCrystalSystem = crystalSystem;
+        cardOccurrence = occurrence;
+        cardHardness = hardness;
+        cardSpecificGravity = specificGravity;
+        cardCleavage = cleavage;
+        cardCrystalAbundance = crystalAbundance;
+        cardEconomicValue = economicValue;
+    }
+
+}
+
---
+++ src/MineralCard.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Darcie on 26/08/2016.*/
+public class MineralCard extends Card{
+
+    public MineralCard(String title, String chemistry, String classification,
+                       String crystalSystem, String occurrence, double hardness,
+                       double specificGravity, String cleavage, String crystalAbundance,
+                       String economicValue){
+        super(title, chemistry, classification, crystalSystem, occurrence, hardness, specificGravity,
+                cleavage, crystalAbundance, economicValue);
+    }
+}
